9e23968f9fe09008cfa4ce12d9426bcf715e7684
Haley Holcomb <haleyholcomb@Haleys-MacBook-Pro.local>
2019-06-20 18:19:10 -0500

    imported csv file, started dictionary for data with headers as keys

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,28 @@
-#main 
+#main
+import os
+import csv
+
+#importing the CSV file
+csvpath = "../data/budget_data.csv"
+with open ("../data/budget_data.csv", "r") as csvfile:
+    budget_data = csv.reader (csvfile, delimiter=",")
+
+    #getting headers
+    headers = next(budget_data)
+
+    data = {}
+
+    for h in headers:
+        data[h] = []
+
+    print(data)
+
+    #for row in budget_data:
+        #print(row)
+
+#keeping main code seperate
+def main():
+    print("this is the main file")
+    print("Financial Analysis")
+
+main()
